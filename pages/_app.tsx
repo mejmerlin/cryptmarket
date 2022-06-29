00f2c7e47a2129565e913f3cb84278f3abd90505
mejmerlin <michaelejeh11@gmail.com>
2022-06-29 22:30:12 +0100

    cryptmarket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+import { CoinMarketProvider } from '../context/context'
+import { GunProvider } from '../context/gunContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log(process.env.NEXT_PUBLIC_SERVER)
+  console.log(process.env.NEXT_PUBLIC_APP_ID)
+  return (
+    <MoralisProvider
+    serverUrl="https://qslkqeofyca3.usemoralis.com:2053/server"
+    appId={"pIRJIoJmirOvnF1BPk0ja1dMHctxpfFrEvUyD2G3"}
+    >
+      <GunProvider >
+        <CoinMarketProvider>
+            <Component {...pageProps} />
+        </CoinMarketProvider>
+      </GunProvider>
+
+
+    </MoralisProvider>  
+  )
 }
 
 export default MyApp
